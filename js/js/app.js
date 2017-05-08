0a61a42f3fe5d575534b06d685766bf8f999be97
sabin <nibaserc@gmail.com>
2017-05-08 12:36:26 -0500

    Data between two controller shared using factory

--- js/js/app.js
+++ js/js/app.js
@@ -4,7 +4,8 @@ var app = angular.module('myApp', ['ngRoute','ngSanitize']);
 app.config(['$routeProvider',function ($routeProvider) {
     $routeProvider
       .when('/', {
-        template: '<h1>Home</h1><a href="#!/aboutMe">About Me</a>'
+       templateUrl:'templates/aboutMeTemplate/aboutMe.html',
+        controller:'aboutMeCtrl'
       })
         .when('/coreJava',{
            template:'<h2>I am Nikita Karki</h2>'
@@ -20,3 +21,52 @@ app.config(['$routeProvider',function ($routeProvider) {
 }]);
 
 app.filter('showInHtml', function($sce) { return $sce.trustAsHtml; })
+
+
+app.factory('myFactory', function(){
+    var myObject={};
+    myObject.message='';
+  
+     return{
+        callFactory:function(msg){
+                myObject.message=msg;
+                return myObject;
+        }
+    }
+});
+
+app.controller('mainCtrl', function( $scope, myFactory ){
+    $scope.showInMainController=myFactory.callFactory();
+    
+});
+
+
+
+/*One way of sharing data between two controller using $broadcast*/
+/*app.controller('mainCtrl',['$scope','myFactory',function($scope,myFactory,$rootScope){
+      $scope.$on('handleBroadcast', function(){
+             $scope.showInMainController = myFactory.message;
+     });
+      
+    $scope.test=function(){
+        $scope.showInMainController = myFactory.message;
+    }
+
+}]);
+
+
+app.factory('myFactory',['$rootScope',function($rootScope,mainCtrl){
+    var myObject={};
+    myObject.message='';
+    myObject.prepForBroadcast=function(msg){
+                       this.message=msg;
+                        this.broadcast();
+    };
+    myObject.broadcast=function(){
+        $rootScope.$broadcast('handleBroadcast');
+    }
+    return myObject;
+ }]);*/
+
+
+
--- js/js/controller/aboutMeCtrl.js
+++ js/js/controller/aboutMeCtrl.js
@@ -1,10 +1,20 @@
-angular.module('myApp').controller('aboutMeCtrl',['$scope', '$sce',function($scope,$sce){
-
+angular.module('myApp').controller('aboutMeCtrl',['$scope', 'myFactory',function($scope,myFactory){
     
-    $scope.aboutMyself=function(){   
+    $scope.aboutMyself = function(){   
+        var msg="<h2>Hello Sabin</h2>";
+         myFactory.callFactory(msg);
+    };
+       
+       $scope.aboutMyself();
     
-        $scope.aboutMyselfR="<h2>Hello Sabin</h2>";
+    $scope.aboutMyLastProject=function(){
+            var msg="<h1>My Lst project</h1>";
+          myFactory.callFactory(msg);
+    };
+    
+    $scope.aboutChallenging=function(){
+             var msg="<h1>My Challenging project</h1>";
+          myFactory.callFactory(msg);
     }
-    
     
 }]);
